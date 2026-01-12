94c63e987aa286fb8a7ab0628c29bcf206fb3efa
Rodrigo Barrios <rodrigobarrios_10@hotmail.com>
2026-01-12 00:55:52 -0300

    components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,12 @@ import './App.css'
 function App() {
   return (
     <h1>Holaa React</h1>
+    
   );
+};
+
+const Button = ({ label, onClick }) => {
+  return <button onClick={onClick}>{label}</button>;
 };
 
 export default App;

50e01c413b6b2eea9b9ce8edfc802bb75b524ae3
Rodrigo Barrios <rodrigobarrios_10@hotmail.com>
2026-01-12 02:32:45 -0300

    Bootstrap agregado a la landing

--- my-react-app/src/components/NavBar.jsx
+++ my-react-app/src/components/NavBar.jsx
@@ -2,18 +2,32 @@ import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
-        <nav>
-            <h2>Mi Tienda</h2>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+            <a className="navbar-brand" href="#">
+            Mi Tienda
+            </a>
 
-            <ul>
-                <li>Inicio</li>
-                <li>Productos</li>
-                <li>Contacto</li>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <a className="nav-link active" href="#">
+                Inicio
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">
+                Productos
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">
+                Contacto
+                </a>
+            </li>
             </ul>
 
             <CartWidget />
         </nav>
-    );
+        );
 };
 
 export default NavBar;
